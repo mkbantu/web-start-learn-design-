a6cd24f1b1194f24abd94b8ead4a83e2fc5de496
mkbantu <mkbantu56@gmail.com>
2026-05-28 15:40:09 +0200

    removing unwanted code and finilising

--- nav.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const toggle = document.querySelector('.nav-toggle');
-  const menu = document.querySelector('nav ul');
-  if (toggle && menu) {
-    toggle.addEventListener('click', function () {
-      menu.classList.toggle('open');
-    });
-  }
-});
